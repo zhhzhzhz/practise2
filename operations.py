786dd56a07cce0910bbcb9ccaba872cb6efd0598
zhhzhzhz <am_omirbek@kbtu.kz>
2026-02-07 10:22:01 +0500

    Add files via upload

---
+++ operations.py
@@ -0,0 +1,33 @@
+print(10 + 5)
+
+sum1 = 100 + 50      # 150 (100 + 50)
+sum2 = sum1 + 250    # 400 (150 + 250)
+sum3 = sum2 + sum2   # 800 (400 + 400)
+
+x = 5
+y = 3
+
+print(x == y)
+print(x != y)
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)
+
+x = 5
+
+print(1 < x < 10)
+
+print(1 < x and x < 10)
+
+x = 5
+
+print(x > 0 and x < 10)
+
+x = ["apple", "banana"]
+y = ["apple", "banana"]
+z = x
+
+print(x is z)
+print(x is y)
+print(x == y)
