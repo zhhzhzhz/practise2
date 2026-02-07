30c0e441ddfed829ea5a72746c4606c2f1528915
zhhzhzhz <am_omirbek@kbtu.kz>
2026-02-07 10:41:31 +0500

    Add files via upload

---
+++ whileloop.py
@@ -0,0 +1,18 @@
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+
+i = 1
+while i < 6:
+  print(i)
+  if i == 3:
+    break
+  i += 1
+
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+else:
+  print("i is no longer less than 6")
