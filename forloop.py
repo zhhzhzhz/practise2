d314ca901e0d2bba5c13f4df39745f22027fe4d2
zhhzhzhz <am_omirbek@kbtu.kz>
2026-02-07 10:46:25 +0500

    Add files via upload

---
+++ forloop.py
@@ -0,0 +1,31 @@
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+
+for x in "banana":
+  print(x)
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+  if x == "banana":
+    break
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    break
+  print(x)
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    continue
+  print(x)
+
+for x in range(6):
+  print(x)
+
+for x in range(2, 6):
+  print(x)
+
